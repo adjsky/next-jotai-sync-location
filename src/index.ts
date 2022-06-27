adee3d154904c157fe5176661be7ac922a6532c2
adjsky <igorlfmartins@mail.ru>
2022-06-27 13:38:36 +0300

    Serialization / deserialization & peer dependencies & package.json files

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,37 @@ import Router from "next/router"
 import { atomWithStorage, RESET } from "jotai/utils"
 
 import type { WritableAtom, SetStateAction } from "jotai"
+import type { UrlObject } from "url"
 
-type UrlQueryInput =
-  | string
-  | number
-  | boolean
-  | ReadonlyArray<string>
-  | ReadonlyArray<number>
-  | ReadonlyArray<boolean>
-  | null
+const push = (url: UrlObject) =>
+  Router.push(url, undefined, {
+    scroll: false
+  })
 
-const atomWithLocation = (
+const replace = (url: UrlObject) =>
+  Router.replace(url, undefined, {
+    scroll: false
+  })
+
+const atomWithLocation = <T>(
   key: string,
-  initialValue: UrlQueryInput,
+  initialValue: T,
   options?: {
     delayInit?: boolean
     replaceState?: boolean
   }
-): WritableAtom<UrlQueryInput, SetStateAction<UrlQueryInput> | typeof RESET> =>
-  atomWithStorage(key, initialValue, {
+): WritableAtom<T, SetStateAction<T> | typeof RESET> => {
+  const serialize = (value: T) =>
+    Array.isArray(value)
+      ? value.map((item) => JSON.stringify(item))
+      : JSON.stringify(value)
+
+  const deserialize = (value: string | string[]) =>
+    Array.isArray(value)
+      ? value.map((item) => JSON.parse(item))
+      : JSON.parse(value)
+
+  return atomWithStorage(key, initialValue, {
     getItem: (key) => {
       const value = Router.query[key]
 
@@ -28,32 +40,37 @@ const atomWithLocation = (
         throw new Error("no value stored")
       }
 
-      return value
+      return deserialize(value)
     },
     setItem: (key, newValue) => {
       const url = {
         pathname: Router.pathname,
-        query: { ...Router.query, [key]: newValue }
+        query: {
+          ...Router.query,
+          [key]: serialize(newValue)
+        }
       }
 
-      options?.replaceState ? Router.replace(url) : Router.push(url)
+      options?.replaceState ? replace(url) : push(url)
     },
     removeItem: (key) => {
       const query = { ...Router.query }
 
       delete query[key]
 
-      Router.push({
+      const url = {
         pathname: Router.pathname,
         query
-      })
+      }
+
+      options?.replaceState ? replace(url) : push(url)
     },
     subscribe: (key, setValue) => {
       const callback = () => {
         const value = Router.query[key]
 
         if (value !== undefined) {
-          setValue(value)
+          setValue(deserialize(value))
         } else {
           setValue(initialValue)
         }
@@ -67,5 +84,6 @@ const atomWithLocation = (
     },
     ...(options?.delayInit && { delayInit: true })
   })
+}
 
 export default atomWithLocation
